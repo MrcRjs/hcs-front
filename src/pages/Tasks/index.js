b4f3a3593d8d620dd9b6257333555aa59dddff9b
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-10-11 01:09:52 -0400

    [WIP] Task card styling

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Fade from 'react-bootstrap/Fade'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 import AuthService from '../../services/AuthService';
 
@@ -24,12 +26,12 @@ const Tasks = props => {
         setNewTask(newDescription.target.value);
     };
 
-    const handleCreateNewTask = async() => {
+    const handleCreateNewTask = async () => {
         return Auth.fetch('/tasks', {"method": "POST", body: {title: newTask}})
             .then(res => {
                 console.log(res);
                 setNewTask('');
-                setTaskList(res.tasks)
+                setTaskList(res.tasks.reverse())
             })
             .catch(e => {
                 console.log(e);
@@ -40,32 +42,56 @@ const Tasks = props => {
     useEffect(() => {
         if (!taskList) {
             Auth.fetch('/tasks', {"method": "GET"}).then(response => {
-                handleTaskList(response.tasks);
+                handleTaskList(response.tasks.reverse());
             });
         }
     });
 
-    const Tasks = !taskList || taskList.length === 0 ?
-        <Container variant={"success"}><p>You don't have any tasks, create one!</p></Container> : taskList.map((t, i) => {
+    let Tasks;
+    if (!taskList) {
+        Tasks = [1, 2].map(i => <Fade appear in key={i}><Card bg={'light'} text="muted" style={{marginBottom: '1rem'}}>
+            <Card.Header>{"..."}</Card.Header><Card.Body></Card.Body></Card></Fade>);
+    } else if (taskList.length === 0)
+        Tasks = <Container variant={"success"}><p>You don't have any tasks, create one!</p></Container>;
+    else {
+        Tasks = taskList.map((t, i) => {
             return (
-                <Card key={t._id}>
-                    <p>{t.title}</p>
-                </Card>);
+                <Fade appear in key={t._id}><Card bg={'light'} style={{marginBottom: '1rem'}}>
+                    <Card.Header>
+                        <Row className="justify-content-between align-items-center">
+                            {new Date(t.created).toLocaleString()}
+                            <ButtonGroup aria-label="Basic example">
+                                <Button size={'sm'} variant="outline-primary">Edit</Button>
+                                <Button size={'sm'} variant="outline-danger">Delete</Button>
+                            </ButtonGroup>
+                        </Row>
+                    </Card.Header>
+                    <Card.Body><Card.Text text={'primary'}><strong>{t.title}</strong></Card.Text></Card.Body>
+                </Card></Fade>);
         });
+    }
     return (
         <Container className={"Tasks"}>
             <Row>
                 <Col sm={12} md={{span: 6, offset: 3}}>
-                    <Container>{Tasks}</Container>
-                    <Form.Group>
-                        <Form.Label>Create new task</Form.Label>
-                        <Form.Control type="text" placeholder="Task description" value={newTask} onChange={handleOnChangeNewTask} />
-                    </Form.Group>
-                    <Button onClick={handleCreateNewTask}>Create task</Button>
+
+                    {Tasks}
+                    <hr/>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Create new task</Form.Label>
+
+                            <Form.Control type="text" placeholder="Task description" value={newTask}
+                                          onChange={handleOnChangeNewTask}/>
+
+                        </Form.Group>
+
+                        <Button variant="outline-primary" onClick={handleCreateNewTask}>Create task</Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
-);
+    );
 };
 
 export default Tasks;
